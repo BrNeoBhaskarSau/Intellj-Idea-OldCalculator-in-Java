383259335e399bd2845d32acacb875975ef5cf94
BrNeoBhaskarSau <proscoders@gmail.com>
2023-04-11 02:42:35 +0530

    Second Commit for this project

---
+++ src/PwCode/HelloWorld.java
@@ -0,0 +1,14 @@
+package PwCode;
+
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello World! code with bhaskarsau.../");
+        for(int i=1; i<=6; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
